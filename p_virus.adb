6e3194b282a4bb8a310e068531dbd66a2e3d9bba
ElsaBn <elsa.bernet38@gmail.com>
2021-02-01 11:21:19 +0100

    proceduremaj_grille2
    normalement c'est juste
    
    (de son prénom)

--- p_virus.adb
+++ p_virus.adb
@@ -34,11 +34,28 @@ package body p_virus is
 	-- {la pièce de couleur coul peut être déplacée dans la direction Dir}
 	--	=> {Grille a été mis à jour suite au deplacement}
 	begin
-		if 
-		hg: -1 -1 T_Lig(pos-1) T_Col(pos-1)
-		hd: -1 +1 T_Lig(pos-1) T_Col(pos+1)
-		bg: +1 -1 T_Lig(pos+1) T_Col(pos-1)
-		bd: +1 +1 T_Lig(pos+1) T_Col(pos+1)
+		for i in TV_Grille'range(1) loop -- naviguation a travers les lignes de la grille
+			for j in TV_Grille'range(2) loop-- naviguation a travers les colonnes de la grille
+					if Grille(i,j) = coul then --sélectionne la case avec la pièce de la bonne coul
+						if Dir = hg then
+							Grille(i-1,j-1) := coul;	--déplacement vers le haut (ligne-1) et la gauche (colonne -1)
+							Grille(i,j) := VIDE; --la case est de nouveau vide
+
+						elsif Dir = hd then
+							Grille(i-1,j+1) := coul; --haut (ligne-1), droite (colonne+1)
+							Grille(i,j) := VIDE;
+
+						elsif Dir = bg then
+							Grille(i+1,j-1) := coul; --bas (ligne+1), droite (colonne-1)
+							Grille(i,j) := VIDE;
+
+						elsif Dir = hd then
+							Grille(i+1,j+1) := coul; --bas (ligne+1), droite (colonne+1)
+							Grille(i,j) := VIDE;
+						end if;
+					end if;
+				end loop;
+			end loop;
 	end MajGrille;
 
 	function Guerison(Grille : in TV_Grille) return boolean is
