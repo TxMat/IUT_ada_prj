0e1719eded409bd2357ae59e90e94b8cc76bac86
ArnouxE <78238358+ArnouxE@users.noreply.github.com>
2021-02-01 10:13:11 +0100

    Update p_virus.adb

--- p_virus.adb
+++ p_virus.adb
@@ -3,7 +3,22 @@ with p_esiut; use p_esiut;
 with sequential_io;
 
 package body p_virus is
-	
-	
-	
+
+	procedure PosPiece(Grille : in TV_Grille; coul : in T_coulP) is
+	-- {} => {la position de la pièce de couleur coul a été affichée, si coul appartient à Grille:
+	--				exemple : ROUGE : F4 - G5}
+
+
+	begin
+	ecrire(coul & " : ");
+	for i in TV_Grille'range(1) loop -- naviguation a travers les lignes de la grille
+		for j in TV_Grille'range(2) loop-- naviguation a travers les colonnes de la grille
+				if Grille(i,j) = coul then
+					ecrire(image(j) & image(i) & " ");
+				end if;
+		end loop;
+	end loop;
+
+	end PosPiece;
+
 end p_virus;
