ddefd6b4d8f9f6e3a05404295b85ab5534ac8f4c
TxMat <mathieu.ponal@gmail.com>
2021-02-01 09:56:54 +0100

    ntm elsa
    fix conflict

--- testjeu.adb
+++ testjeu.adb
@@ -1,8 +1,14 @@
 with p_virus; use p_virus;
 
 procedure main is
-	-- declare here
+    Grille : TV_Grille;
+    Pieces : TV_Pieces;
 begin
-	-- code here
-	--NON
+    put_line("Debut de la phase d'initialisation");
+    InitPartie(Grille, Pieces);
+    put_line("Initialisation terminée");
+    put_line("Entrez un numero de configuration [1-20]");
+
+exception
+
 end main;
