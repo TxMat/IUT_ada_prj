565c2762bc02fd0ace13b1857be2118fe7660321
ElsaBn <elsa.bernet38@gmail.com>
2021-02-03 15:48:20 +0100

    pitites corrections textes

--- av_txt.adb
+++ av_txt.adb
@@ -26,20 +26,20 @@ procedure av_txt is
     ANNUL_MOVE : exception;
 begin
         -- INIT grille
-        put_line("Debut de la phase d'initialisation");
+        put_line("Début de la phase d'initialisation");
         InitPartie(Grille, Pieces);
         put_line("Initialisation terminée");
         -- Demande num config
         loop
             begin
-                put_line("Entrez un numero de niveau [1-20]");
+                put_line("Entrez un numéro de niveau [1-20] : ");
                 get(num_conf);
                 skip_line;
                 exit;
             exception
                 when others => -- en cas de mauvaise saisie
                     skip_line;
-                    put_line("Le numéro est invalide vous devez mettre un nombre");
+                    put_line("Le numéro est invalide, vous devez mettre un nombre");
                     put_line("entre 1 et 20");
             end;
         end loop;
@@ -49,7 +49,7 @@ begin
             nb_coups := 0;
             Premier_tour := True;
             -- Debut config
-            put_line("Debut de la phase de configuration");
+            put_line("Début de la phase de configuration");
             Configurer(f, num_conf, Grille, Pieces);
             put_line("Configuration terminée");
             -- Debut partie
@@ -61,7 +61,7 @@ begin
                     a_la_ligne;
                     if not Mode_Opti then
                         put_line("Choisissez une action :");
-                        put_line("  -- [p] bouger une piece");
+                        put_line("  -- [p] bouger une pièce");
                         put_line("  -- [x] mode jeu optimisé");
                         put_line("  -- [a] annuler le dernier mouvement");
                         put_line("  -- [r] recommencer");
@@ -75,7 +75,7 @@ begin
                     -- case non supporté avec les types sting on utilise un if --
                     if rep = "p" or Mode_Opti then -- Deplacement pieces
                         loop -- loop si piece invalide
-                            put_line("Choissisez le numero d'une piece a deplacer");
+                            put_line("Choisissez le numero d'une pièce à déplacer");
                             loop -- loop de saisie du numero
                                 begin
                                     lire(num_coul); -- demande couleur
@@ -94,7 +94,7 @@ begin
                                                     raise OPTI_OFF; -- on gere l'erreur sur le bloc principal pour repartir au debut                                                        -- put_line("Entrez un le numero d'une piece a deplacer");
                                                     -- exit;
                                                 elsif rep = "n" then
-                                                    put_line("Dans ce cas mettez le numero d'une piece entre 0 et 8 sur la grille !");
+                                                    put_line("Dans ce cas mettez le numéro d'une pièce entre 0 et 8 sur la grille !");
                                                     exit;
                                                 elsif rep = "a" then
                                                     if not Premier_tour then -- pour verif qu'on a joué au moins une fois
@@ -106,16 +106,16 @@ begin
                                                         put_line("Vous ne pouvez rien annuler car vous n'avez pas joué !");
                                                     end if;
                                                 else
-                                                    put_line("repondez avec 'y' ou 'n'");
+                                                    put_line("Répondez avec 'y' ou 'n'");
                                                 end if;
                                             end loop;
                                         else
-                                            put_line("La couleur doit etre contenue entre 0 et 8 !");
+                                            put_line("Le chiffre de la couleur doit être contenu entre 0 et 8 !");
                                         end if;
                                 end;
                             end loop;
                             if Pieces(coul) and coul /= T_coulP'last and checkpossible(Grille, coul) then -- si la couleur est dans la gille et n'est pas blanche on entre
-                                put_line("dans quelle direction voulez vous deplacer la piece ? [hg / bg / bd / hd]");
+                                put_line("Dans quelle direction voulez vous déplacer la pièce ? [hg / bg / bd / hd]");
                                 lire(dir);
                                 a_la_ligne;
                                 if Possible(Grille, coul, dir) then -- check si Possible + deplacement
@@ -126,15 +126,15 @@ begin
                                     exit;
                                 else -- si Possible renvoie False
                                     nb_err := nb_err + 1;
-                                    put_line("deplacement impossible");
+                                    put_line("Déplacement impossible");
                                 end if;
                             elsif coul = T_coulP'last then -- message d'erreur piece blanche
-                                put_line("Les pieces blanches ne peuvent pas etre deplacées");
+                                put_line("Les pièces blanches ne peuvent pas être déplacées");
                             elsif not checkpossible(Grille, coul) then
                                 nb_err := nb_err + 1;
-                                put_line("cette piece ne peut bouger nul part");
+                                put_line("Cette pièce ne peut bouger nulle part");
                             else -- message d'erreur piece inexistante
-                                put_line("Cette piece n'existe pas dans la configuration actuelle");
+                                put_line("Cette pièce n'existe pas dans la configuration actuelle");
                             end if;
                         end loop;
                     elsif rep = "a" then  -- Undo
@@ -147,8 +147,8 @@ begin
                         end if;
                     elsif rep = "r" then -- Restart
                         put_line("/!\ VOULEZ VOUS VRAIMENT RECOMMENCER ? /!\");
-                        put_line("il est impossible de revenir en arriere");
-                        put_line("'y' pour confirmer nimporte quoi d'autre pour annuler");
+                        put_line("Il est impossible de revenir en arrière");
+                        put_line("'y' pour confirmer, n'importe quoi d'autre pour annuler");
                         lire(rep);
                         if rep = "y" then
                             nb_err := 0;
@@ -158,19 +158,19 @@ begin
                         end if;
                     elsif rep = "q" then
                         put_line("=======================================");
-                        put_line("---------vous avez abandonné :(--------");
-                        put_line("-- apres" & integer'image(nb_coups) & " coups");
+                        put_line("---------Vous avez abandonné :(--------");
+                        put_line("-- Après" & integer'image(nb_coups) & " coups");
                         put_line("-- et" & integer'image(nb_err) & " erreurs");
                         put_line("=======================================");
                         exit;
                     elsif rep = "x" then
                         put_line("-- Bienvenue dans le mode optimisé --");
-                        put_line("vous n'aurez pas a taper P a chaque fois pour jouer une piece!");
-                        put_line("mettez un numero de piece invalide pour en sortir ()");
+                        put_line("Vous n'aurez pas à taper P à chaque fois pour jouer une pièce !");
+                        put_line("Mettez un numéro de pièce invalide pour en sortir (supérieur à 8");
                         pause;
                         Mode_Opti := True;
                     else
-                        put_line("relisez bien les actions possibles celle que vous avez mis n'existe pas");
+                        put_line("Relisez bien les actions possibles, celle que vous avez mis n'existe pas");
                     end if;
                 exception
                     when OPTI_OFF =>
@@ -178,20 +178,20 @@ begin
                     put_line("__Mode optimisé off__");
 
                     when ANNUL_MOVE =>
-                    put_line("mouvement annulé");
+                    put_line("-> Mouvement annulé");
                 end;
             end loop;
             if Guerison(Grille) then -- car Possible de sortir de la boucle pour abandonner
                 put_line("=======================================");
                 put_line("-------------__BRAVO !!!__-------------");
-                put_line("-- Vous avez gagné apres" & integer'image(nb_coups) & " coups");
+                put_line("-- Vous avez gagné après" & integer'image(nb_coups) & " coups");
                 put_line("-- et" & integer'image(nb_err) & " erreurs");
                 put_line("=======================================");
             end if;
             if num_conf = 20 then
                 if Guerison(grille) then
                     put_line("BRAVO VOUS AVEZ FINI LE JEU !!");
-                    put_line("Nous esperons qu'il vous a plu :)");
+                    put_line("Nous espèrons qu'il vous a plu :)");
                     put_line("Vous pouvez refaire les anciens niveaux");
                     exit;
                 else
@@ -209,12 +209,12 @@ begin
                         if Guerison(grille) then
                             num_conf := num_conf + 1;
                         end if;
-                        put_line("pensez a repartir du niveau" & Integer'image(num_conf) & " pour continuer le jeu");
+                        put_line("Pensez à repartir du niveau" & Integer'image(num_conf) & " pour continuer le jeu");
                         put_line("Bonne journée :)");
                         Play := False;
                         exit;
                     else
-                        put_line("repondez avec 'y' ou 'n'");
+                        put_line("Répondez avec 'y' ou 'n'");
                     end if;
                 end loop;
             end if;
