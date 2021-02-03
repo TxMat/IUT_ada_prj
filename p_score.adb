9d2d96b6da14142908d09907c519b2c5f7624dde
dvquentincarrel <78238441+dvquentincarrel@users.noreply.github.com>
2021-02-03 20:43:10 +0100

    Résolution des bugs
    + Correction syntaxe pour consistency

--- p_score.adb
+++ p_score.adb
@@ -1,13 +1,15 @@
 with sequential_io;
-
+with p_esiut; use p_esiut;
 package body p_score is
 
-    function analyse_fichier(mode,defi : in integer := 0; nom : in string := ""; tableau_score : in out TV_Score; f : in out p_score_io.file_type) return integer is
+    function analyse_fichier(f : in out p_score_io.file_type; mode,defi : in integer := 0; nom : in string := ""; tableau_score : in out TV_Score) return integer is
         --nom param => val
         tmp : TR_Score;
         i : integer := 0;
+        f_nom : string := name(f);
     begin
-        reset(f);
+        close(f);
+        open(f,in_file,f_nom);
         while not end_of_file(f) loop
             read(f,tmp);
             if  (mode = 1 and then ((tmp.nom /= nom) and (tmp.defi /= defi))) or -- ajout_score
@@ -18,25 +20,18 @@ package body p_score is
                 tableau_score(i) := tmp;
             end if;
         end loop;
+        close(f);
+        open(f,append_file,f_nom);
         return i;
     end analyse_fichier;
 
     -- Écriture du score dans le fichier
-    procedure ajout_score(score : in TR_Score; f : in out p_score_io.file_type) is
+    procedure ajout_score(f : in out p_score_io.file_type; score : in TR_Score) is
         scores_existants : TV_Score(1..NB_SCORE_MAX);
         i : integer := 0;
     begin
 
-        -- Récupère tous les scores n'étant pas celui à ajouter -------
-        -- while not end_of_file(f) loop
-        --     read(f,tmp);
-        --     if (tmp.nom /= score.nom) and (tmp.defi /= score.defi) then
-        --         i := i + 1;
-        --         scores_existants(i) := tmp;
-        --     end if;
-        -- end loop;
-        i := analyse_fichier(1, 0, "", scores_existants, f);
-        ---------------------------------------------------------------
+        i := analyse_fichier(f, 1, 0, "", scores_existants);
 
         -- Ajoute le nouveau score ------------------------------------
         i := i + 1;
@@ -53,39 +48,43 @@ package body p_score is
     end ajout_score;
 
     -- ouverture du fichier score, création sinon inexsitant
-    procedure ouvrir_fichier(name : in string; f : in out p_score_io.file_type) is
+    procedure ouvrir_fichier(f : in out p_score_io.file_type; file_name : in string) is
     begin
-        open(f, append_file, name);
+        open(f, append_file, file_name);
     exception
         when NAME_ERROR => -- Si le fichier n'existe pas
-            create(f, out_file, name);
+            create(f, out_file, file_name);
     end ouvrir_fichier;
 
     -- Recupére les scores enregistrés pour ce défi
-    function recup_score(defi : in integer; f : in out p_score_io.file_type) return TV_Score is
+    function recup_score(f : in out p_score_io.file_type; defi : in integer) return TV_Score is
         scores_existants : TV_Score(1..NB_SCORE_MAX);
         i : integer := 0;
     begin
-        i := analyse_fichier(2,defi,"",scores_existants,f);
-        return scores_existants(1..i);
+        i := analyse_fichier(f,2,defi,"",scores_existants);
+        return scores_existants(1..NB_SCORE_MAX);
     end recup_score;
 
     -- Récupére les scores enregistrés pour ce joueur
-    function recup_score(nom : in string; f : in out p_score_io.file_type) return TV_Score is
+    function recup_score(f : in out p_score_io.file_type; nom : in string) return TV_Score is
         scores_existants : TV_Score(1..NB_SCORE_MAX);
         i : integer := 0;
     begin
-        i := analyse_fichier(3,0,nom,scores_existants,f);
-        return scores_existants(1..i);
+        i := analyse_fichier(f,3,0,nom,scores_existants);
+        return scores_existants;
     end recup_score;
 
     -- Récupére le score du joueur pour ce défi
-    function recup_score(infos : in TR_Score; f : in out p_score_io.file_type) return TR_Score is
+    function recup_score(f : in out p_score_io.file_type; infos : in TR_Score) return TR_Score is
         scores_existants : TV_Score(1..NB_SCORE_MAX);
         i : integer := 0;
     begin
-        i := analyse_fichier(4,infos.defi,infos.nom,scores_existants,f);
+        i := analyse_fichier(f,4,infos.defi,infos.nom,scores_existants);
         return scores_existants(1);
     end recup_score;
+
+    procedure afficher_scores(scores : in TV_Score) is
+    begin
+    end afficher_scores;
 
 end p_score;
--- p_score.ads
+++ p_score.ads
@@ -17,21 +17,24 @@ package p_score is
     -- Instanciation sequential_io pour stocker les scores
     package p_score_io is new sequential_io(TR_Score); use p_score_io;
 
-    function analyse_fichier(mode,defi : in integer := 0; nom : in string := ""; tableau_score : in out TV_Score; f : in out p_score_io.file_type) return integer;
+    function analyse_fichier(f : in out p_score_io.file_type; mode,defi : in integer := 0; nom : in string := ""; tableau_score : in out TV_Score) return integer;
 
     -- Écriture du score dans le fichier
-    procedure ajout_score(score : in TR_Score; f : in out p_score_io.file_type);
+    procedure ajout_score(f : in out p_score_io.file_type; score : in TR_Score);
 
     -- ouverture du fichier score, création sinon
-    procedure ouvrir_fichier(name : in string; f : in out p_score_io.file_type);
+    procedure ouvrir_fichier(f : in out p_score_io.file_type; file_name : in string);
 
     -- Recupére les scores enregistrés pour ce défi
-    function recup_score(defi : in integer; f : in out p_score_io.file_type) return TV_Score;
+    function recup_score(f : in out p_score_io.file_type; defi : in integer) return TV_Score;
 
     -- Récupére les scores enregistrés pour ce joueur
-    function recup_score(nom : in string; f : in out p_score_io.file_type) return TV_Score;
+    function recup_score(f : in out p_score_io.file_type; nom : in string) return TV_Score;
 
     -- Récupére le score du joueur pour ce défi
-    function recup_score(infos : in TR_Score; f : in out p_score_io.file_type) return TR_Score;
+    function recup_score(f : in out p_score_io.file_type; infos : in TR_Score) return TR_Score;
+
+    -- Pour debug & co
+    procedure afficher_scores(scores : in TV_Score);
 
 end p_score;
