f995c6a81cdafd348662e748d4032256376c6137
ponalm <mathieu.ponal@etu.univ-grenoble-alpes.fr>
2022-04-22 22:44:29 +0200

    init grid

--- Grid.cpp
+++ Grid.cpp
@@ -6,5 +6,12 @@
 
 Grid::Grid(int width, int height) :
         _width(width), _height(height) {
-
+    // Initialize the grid with empty cells
+    for (int i = 0; i < _height; i++) {
+        std::vector<Cell> row;
+        for (int j = 0; j < _width; j++) {
+            row.push_back(Cell());
+        }
+        _grid.push_back(row);
+    }
 }
